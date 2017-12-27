d4d05a93007ea313f98cc0019b2c21fd32ab9d6e
Lv Jingyao <jingyao.lv@qq.com>
2017-12-28 00:11:04 +0800

    Initial Commit

---
+++ OCDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OCDemo
+//
+//  Created by y on 2017/12/28.
+//  Copyright © 2017年 willshine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
